0022c0cd5aa76a37587492a1328f704c6caae666
Artak <abrahamyan@outlook.com>
2023-06-25 00:17:35 +0400

    Initial Commit

---
+++ SwiftUI-List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI-List
+//
+//  Created by Artak Abrahamyan on 25.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
